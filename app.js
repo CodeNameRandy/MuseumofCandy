9a727c4ee2a80d9bcf33a19a52f7405e3b0303d9
Randy Randelman <tywengert93@gmail.com>
2019-03-04 01:54:37 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started...");
+});
